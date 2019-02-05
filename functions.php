d3511602a1102f9bc63d0be95d098bd566ccb297
sequeirajonathan <sequeira.s.jonathan@gmail.com>
2019-02-04 20:52:03 -0500

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+function AcmeInc_enqueue() {
+    wp_enqueue_style('semanticUI', 'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css');
+    if($_SERVER['SERVER_NAME'] != 'localhost'){
+      wp_enqueue_style('style', get_template_directory_uri() . '/style.min.css');
+    } else{
+      wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
+    }
+    wp_enqueue_script( 'customjs', get_template_directory_uri() . '/assets/js/custom.min.js', array('jquery'), '', true );
+    wp_enqueue_style('Montserrat', "https://fonts.googleapis.com/css?family=Montserrat:700|Montserrat:normal|Montserrat:300");
+    wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.2.0/css/all.css');
+    wp_enqueue_script( 'bootstrapcdn', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', array('jquery'), '', true );
+}
+add_action('wp_enqueue_scripts', 'AcmeInc_enqueue');
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+<div class="ui secondary  menu">
+  <div class="item">
+    <img src="http://localhost/wordpress/wp-content/uploads/2019/02/Acme-Logo.png" alt="Logo">
+  </div>
+  <div class="right menu">
+  <a class="item">
+    Sales
+  </a>
+  <a class="item">
+    Contact Us
+  </a>
+    <div class="item">
+      <div class="ui icon input">
+        <input type="text" placeholder="Search Products...">
+        <i class="search link icon"></i>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="ui two column doubling stackable grid container">
+    <div class="column">
+        <img src="http://localhost/wordpress/wp-content/uploads/2019/02/iphone.jpg" alt="Iphone">
+        <div class="ui labeled button" tabindex="0">
+            <div class="ui red button">
+                <i class="heart icon"></i>Like</div>
+            <a class="ui basic red left pointing label">1,048</a>
+        </div>
+        <button class="ui green button ">Buy Now</button>
+        <a class="ui teal tag label">Only $900.00!!</a>
+    </div>
+    <div class="column">
+        <p>The innovative solution in the iPhone was to remove the traditional
+            keyboard; instead, there is a touch screen supported by the Multi-Touch
+            technology. No stylus is needed; everything is operated with the use of a
+            finger or multiple fingers. An additional novelty is the display that turns
+            off when brought near the ear (proximity sensor) and a 3.5 in display, with
+            320 x 480 pixels definition, that automatically adjust the screen to the
+            device's orientation.</p>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
